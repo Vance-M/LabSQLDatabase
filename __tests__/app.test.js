98408a74c3be46602ce20a7118ace9ddfb10d023
Vance-M <myveryvills@yahoo.com>
2021-02-20 13:43:53 -0800

    add test and 2nd endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,31 +31,77 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('returns doughnuts', async() => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
+          id: 1,
+          name: 'Glazed',
+          description: 'Classic Glazed Doughnut',
+          specialty: false,
+          price: '0.50',
+          owner_id: 1
         },
         {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
+          id: 2,
+          name: 'Old-Fashioned',
+          description: 'Doughnut made with sour cream and a tapered edge',
+          specialty: false,
+          price: '0.60',
+          owner_id: 1
         },
         {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
+          id: 3,
+          name: 'Cake',
+          description: 'Doughnut made from sweetened dough',
+          specialty: false,
+          price: '0.75',
+          owner_id: 1
+        },
+        {
+          id: 4,
+          name: 'Buttermilk',
+          description: 'Classic doughnut made with buttermilk',
+          specialty: true,
+          price: '1.00',
+          owner_id: 1
+        },
+        {
+          id: 5,
+          name: 'Bearclaw',
+          description: 'Danish filled with almond paste and raisins',
+          specialty: true,
+          price: '1.50',
+          owner_id: 1
+        },
+        {
+          id: 6,
+          name: 'Cruller',
+          description: 'French inspired braided doughnut',
+          specialty: true,
+          price: '1.00',
+          owner_id: 1
+        },
+        {
+          id: 7,
+          name: 'Maple Bar',
+          description: 'Doughnut bar dipped in Maple cream',
+          specialty: false,
+          price: '0.75',
+          owner_id: 1
+        },
+        {
+          id: 8,
+          name: 'Sprinkles',
+          description: 'Chocolate dipped doughnut with sprinkles',
+          specialty: false,
+          price: '0.60',
+          owner_id: 1
         }
       ];
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/doughnuts')
         .expect('Content-Type', /json/)
         .expect(200);
 
